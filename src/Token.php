ba5a56e8d6e95cfaf58fde86eb2bc12546925435
Halil Beycan <halilbeycan0@gmail.com>
2021-12-08 21:39:12 +0300

    first commit

---
+++ src/Token.php
@@ -0,0 +1,228 @@
+<?php
+
+namespace Beycan\MultiChain;
+
+use Web3\Contract;
+use Web3\Validators\AddressValidator;
+use phpseclib\Math\BigInteger as BigNumber;
+
+final class Token
+{
+    /**
+     * Provider
+     * @var Provider
+     */
+    private $provider;
+
+    /**
+     * Current token contract address
+     * @var string
+     */
+    private $address;
+
+    /**
+     * Default gas
+     * @var int
+     */
+    private $defaultGas = 50000;
+
+    /**
+     * Current token contract methods
+     * @var Contract
+     */
+    public $methods;
+
+    /**
+     * @param string $tokenAddress
+     */
+    public function __construct(string $tokenAddress)
+    {
+        if (AddressValidator::validate($tokenAddress) === false) {
+            throw new \Exception('Invalid token address!', 23000);
+        }
+
+        $this->address = $tokenAddress;
+        $this->provider = MultiChain::getProvider();
+        $abi = file_get_contents(dirname(__DIR__) . '/resources/abi.json');
+        $this->methods = (new Contract($this->provider::getHttpProvider(), $abi))->at($tokenAddress);
+    }
+
+    /**
+     * Generates a token transfer data
+     *
+     * @param string $from
+     * @param string $to
+     * @param float $amount
+     * @return array
+     * @throws Exception
+     */
+    public function transferData(string $from, string $to, float $amount) : array
+    {
+        if ($this->getBalance($from) < $amount) {
+            throw new \Exception('Insufficient balance!', 10000);
+        }
+        
+        $hexAmount = Utils::toHex($amount, $this->getDecimals());
+        return [
+            'from' => $from,
+            'value' => '0x0',
+            'to' => $this->address,
+            'chainId' => $this->provider->getChainId(),
+            'nonce' => $this->provider->getNonce($from),
+            'gasPrice' => $this->provider->getGasPrice(),
+            'gas' => $this->getEstimateGas($from, $to, $amount),
+            'data' => '0x' . $this->methods->getData('transfer', $to, $hexAmount),
+        ];
+    }
+
+    /**
+     * Calculates estimated gas for the transfer transaction
+     *
+     * @param string $from
+     * @param string $to
+     * @param float $amount
+     * @return string
+     * @throws Exception
+     */
+    public function getEstimateGas(string $from, string $to, float $amount) : string
+    {
+        $result = null;
+        $amount = Utils::toHex($amount, $this->getDecimals());
+        $this->methods->estimateGas('transfer', $to, $amount, ['from' => $from], function($err, $res) use (&$result) {
+            if ($err) {
+                throw new \Exception($err->getMessage(), $err->getCode());
+            } else {
+                $result = $res;
+            }
+        });
+
+        if ($result instanceof BigNumber) {
+            return Utils::hex($result->toString());
+        } else {
+            return Utils::hex($this->defaultGas);
+        }
+    }
+
+    /**
+     * Returns the token's decimals
+     * @return int
+     * @throws Exception
+     */
+    public function getDecimals() : int
+    {
+        $result = null;
+        $this->methods->call('decimals', function($err, $res) use (&$result) {
+            if ($err) {
+                throw new \Exception($err->getMessage(), $err->getCode());
+            } else {
+                $result = $res;
+            }
+        });
+
+        if (is_array($result) && $result[0] instanceof BigNumber) {
+            return intval($result[0]->toString());
+        } else {
+            throw new \Exception("There was a problem retrieving the decimals value!", 12000);
+        }
+    }
+
+    /**
+     * Returns the balance of the current token in the address given wallet address
+     *
+     * @param string $address
+     * @return float
+     * @throws Exception
+     */
+    public function getBalance(string $address) : float
+    {
+        $result = null;
+        $this->methods->call('balanceOf', $address, function($err, $res) use (&$result) {
+            if ($err) {
+                throw new \Exception($err->getMessage(), $err->getCode());
+            } else {
+                $result = $res;
+            }
+        });
+
+        if (is_array($result) && $result['balance'] instanceof BigNumber) {
+            return Utils::toDec($result['balance']->toString(), $this->getDecimals());
+        } else {
+            throw new \Exception("There was a problem retrieving the balance!", 11000);
+        }
+    }
+
+    /**
+     * get token name
+     *
+     * @return string|null
+     * @throws Exception
+     */
+    public function getName() : ?string
+    {
+        $name = null;
+        $this->methods->call('name', function($err, $res) use (&$name) {
+            if ($err) {
+                throw new \Exception($err->getMessage(), $err->getCode());
+            } else {
+                $name = end($res);
+            }
+        });
+
+        return $name;
+    }
+
+    /**
+     * get token symbol
+     *
+     * @return string|null
+     * @throws Exception
+     */
+    public function getSymbol() : ?string
+    {
+        $symbol = null;
+        $this->methods->call('symbol', function($err, $res) use (&$symbol) {
+            if ($err) {
+                throw new \Exception($err->getMessage(), $err->getCode());
+            } else {
+                $symbol = end($res);
+            }
+        });
+
+        return $symbol;
+    }
+
+    /**
+     * get token total supply
+     *
+     * @return string
+     * @throws Exception
+     */
+    public function getTotalSupply() : string
+    {
+        $totalSupply = null;
+        $this->methods->call('totalSupply', function($err, $res) use (&$totalSupply) {
+            if ($err) {
+                throw new \Exception($err->getMessage(), $err->getCode());
+            } else {
+                $totalSupply = $res;
+            }
+        });
+
+        if (is_array($totalSupply) && end($totalSupply) instanceof BigNumber) {
+            $totalSupply = Utils::toDec(end($totalSupply)->toString(), 
+            $this->getDecimals());
+            return number_format($totalSupply, $this->getDecimals(), ',', '.');
+        } else {
+            throw new \Exception("There was a problem retrieving the total suppy!", 14001);
+        }
+    }
+
+    /**
+     * Returns the current token contract address
+     * @return string
+     */
+    public function getAddress() : string
+    {
+        return $this->address;
+    }
+}
---
+++ src/Utils.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Beycan\MultiChain;
+
+use \Web3\Utils as Web3Utils;
+use phpseclib\Math\BigInteger as BigNumber;
+
+final class Utils
+{
+    /**
+     * @param string $value
+     * @return string
+     */
+    public static function hex($value) : string
+    {
+        return '0x' . dechex($value);
+    }
+
+    /**
+     * Converts the regular number into a format that blockchain networks will understand
+     * Decimal number to hexadecimal number
+     * @param float|int $amount
+     * @param int $decimals
+     * @return string
+     */
+    public static function toHex(float $amount, int $decimals) : string
+    {
+        $value = self::toBigNumber($amount, $decimals);
+        if (is_numeric($value)) {
+            // turn to hex number
+            $bn = self::toBn($value);
+            $hex = $bn->toHex(true);
+            $hex = preg_replace('/^0+(?!$)/', '', $hex);
+        } elseif (is_string($value)) {
+            $value = self::stripZero($value);
+            $hex = implode('', unpack('H*', $value));
+        } elseif ($value instanceof BigNumber) {
+            $hex = $value->toHex(true);
+            $hex = preg_replace('/^0+(?!$)/', '', $hex);
+        } else {
+            throw new InvalidArgumentException('The value to toHex function is not support.');
+        }
+
+        return '0x' . $hex;
+    }
+
+    /**
+     * @param BigNumber|string|int $number
+     * @param int $decimals
+     * @return BigNumber
+     */
+    public static function toBigNumber($number, int $decimals) : BigNumber
+    {
+        $bn = Web3Utils::toBn($number);
+        $length = '1' . str_repeat('0', $decimals);
+
+        $bnt = new BigNumber($length);
+
+        if (is_array($bn)) {
+            list($whole, $fraction, $fractionLength, $negative1) = $bn;
+
+            if ($fractionLength > strlen($length)) {
+                throw new InvalidArgumentException('toBigNumber fraction part is out of limit.');
+            }
+            $whole = $whole->multiply($bnt);
+
+            switch (MATH_BIGINTEGER_MODE) {
+                case $whole::MODE_GMP:
+                    static $two;
+                    $powerBase = gmp_pow(gmp_init(10), (int) $fractionLength);
+                    break;
+                case $whole::MODE_BCMATH:
+                    $powerBase = bcpow('10', (string) $fractionLength, 0);
+                    break;
+                default:
+                    $powerBase = pow(10, (int) $fractionLength);
+                    break;
+            }
+            $base = new BigNumber($powerBase);
+            $fraction = $fraction->multiply($bnt)->divide($base)[0];
+
+            if ($negative1 !== false) {
+                return $whole->add($fraction)->multiply($negative1);
+            }
+            return $whole->add($fraction);
+        }
+
+        return $bn->multiply($bnt);
+    }
+
+    /**
+     * Converts a hexadecimal number to a normal number
+     * Hecadecimal number to decimal number
+     * @param string|int|float $amount
+     * @param int $decimals
+     * @return float
+     */
+    public static function toDec(string $amount, int $decimals) : float
+    {
+        $bn = Web3Utils::toBn($amount);
+        $length = '1' . str_repeat('0', $decimals);
+        $bnt = new BigNumber($length);
+
+        if ($bn->divide($bnt)[0]->toString() != 0) {
+            return (float) $bn->divide($bnt)[0]->toString();
+        } else {
+            $amount = $bn->divide($bnt)[1]->toString();
+            $length = '1' . str_repeat('0', $decimals);
+            return (float) bcdiv($amount, $length, $decimals);
+        }
+    }
+
+}
